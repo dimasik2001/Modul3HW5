97328af33f8ec2283a7ca3b76ddf4b36b4358c3e
Dimasik2001 <dimonkalibaba@gmail.com>
2021-08-12 01:35:30 +0300

    HW

--- Modul3HW5/Modul3HW5/Modul3HW5/Program.cs
+++ Modul3HW5/Modul3HW5/Modul3HW5/Program.cs
@@ -1,14 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Modul3HW5
 {
     public class Program
     {
+        public static async Task<string> ReadHello()
+        {
+            return await File.ReadAllTextAsync("Hello.txt");
+        }
+
+        public static async Task<string> ReadWorld()
+        {
+            return await File.ReadAllTextAsync("World.txt");
+        }
+
+        public static async Task<string> Concat()
+        {
+            var taskList = new List<Task<string>>();
+            taskList.Add(ReadHello());
+            taskList.Add(ReadWorld());
+            return string.Join(' ', await Task.WhenAll(taskList));
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            var str = Concat().GetAwaiter().GetResult();
+            Console.WriteLine("Main:");
+            Console.WriteLine(str);
         }
     }
 }
